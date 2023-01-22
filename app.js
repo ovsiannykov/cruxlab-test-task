8d9de81478d2f977d8ab068bd5d536103c64cf6d
Mykhail Ovsiannykov <93041689+ovsiannykov@users.noreply.github.com>
2023-01-22 11:26:20 +0200

    Added validation logic

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 var fs = require("fs");
 var array = fs.readFileSync("./file-expamle.txt").toString().split("\n");
 for (line in array) {
-  const str = array[line];
-  console.log(str);
+  const latter = array[line].split(" ")[0];
+  const minValue = array[line].split(" ")[1].split("-")[0];
+  const maxValue = array[line].split(" ")[1].split("-")[1].split(":")[0];
+  const pass = array[line].split(" ")[2];
+
+  const matchNumber = pass.split(latter).length - 1;
+
+  if (matchNumber >= minValue && matchNumber <= maxValue) {
+    console.log(pass, true);
+  } else {
+    console.log(pass, false);
+  }
 }
